badfd5aa300fc6f9451f059c20cd0e071ff9aa63
Jorge Lee <jrg.lee@gmail.com>
2023-03-30 12:00:22 -0700

    Some progress in chapter 9

--- src/main/scala/jrglee/fp/exercises/Chapter09.scala
+++ src/main/scala/jrglee/fp/exercises/Chapter09.scala
@@ -23,7 +23,8 @@ object Chapter09 {
     def char(c: Char): Parser[Char] = string(c.toString) map (_.charAt(0))
     def succeed[A](a: A): Parser[A] // = string("") map (_ => a)
     def slice[A](p: Parser[A]): Parser[String]
-    def map2[A, B, C](a: Parser[A], b: => Parser[B])(f: (A, B) => C): Parser[C] = a.flatMap(pa => b.map(pb => f(pa, pb)))
+    def map2[A, B, C](a: Parser[A], b: => Parser[B])(f: (A, B) => C): Parser[C] =
+      a.flatMap(pa => b.map(pb => f(pa, pb)))
     def many1[A](p: Parser[A]): Parser[List[A]] = map2(p, many(p))(_ :: _)
     def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]] =
       if (n == 0) succeed(List.empty)
@@ -41,12 +42,29 @@ object Chapter09 {
       def map[B](f: A => B): Parser[B] = self.map(p)(f)
       def **[B](p2: Parser[B]): Parser[(A, B)] = self.product(p, p2)
       def flatMap[B](f: A => Parser[B]): Parser[B] = self.flatMap(p)(f)
+      def many: Parser[List[A]] = self.many(p)
+      def slice: Parser[String] = self.slice(p)
     }
 
     object Laws {
       def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop = forAll(in)(s => run(p1)(s) == run(p2)(s))
       def mapLaw[A](p: Parser[A])(in: Gen[String]): Prop = equal(p, p.map(a => a))(in)
       def productLaw[A, B](p: Parser[A])(in: Gen[String]): Prop = equal(p ** p, p.map(a => (a, a)))(in)
+    }
+  }
+
+  object JSONParser {
+    def jsonParser[Err, Parser[+_]](P: Parsers[Err, Parser]): Parser[JSON] = {
+      import P._
+
+      val spaces = char(' ').many.slice
+      ???
+    }
+
+    trait JSON
+    object JSON {
+      case object JNull extends JSON
+      case class JNumber(get: Double) extends JSON
     }
   }
 }
